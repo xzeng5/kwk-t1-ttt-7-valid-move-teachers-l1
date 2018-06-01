74a8d5c4f463b10b65dde5500755d37229c0afd8
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-01 14:38:05 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,6 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  elsif board[index] == "X" || board[index] == "O"
-    true
+  else
   end
 end
