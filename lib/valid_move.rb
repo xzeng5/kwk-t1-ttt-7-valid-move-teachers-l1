043343196cd6c30cc8274d4deda7e28049e352fb
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-01 14:33:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false
     true
+  elsif position_taken?(board, index) == true
+    false
   elsif index.between?(0, 8)
   end
 end
